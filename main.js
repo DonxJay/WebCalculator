09a8e4a4a7de61f2e93f02e71c18c0eb6fdafed5
Njabulo <njabulophiri55@gmail.com>
2023-08-25 12:53:31 +0300

    Version 1

---
+++ main.js
@@ -0,0 +1,17 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = 
+        eval(document.getElementById('display').value);
+    } catch {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
